c27330cb8a14086f0d41cd69add059d7becaf6c2
bram-vdberg <bram@cow.fi>
2024-07-03 10:09:51 +0200

    Run code formatter so github actions pass

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,14 @@
 from web3 import Web3
 
-SETTLEMENT_CONTRACT_ADDRESS = Web3.to_checksum_address('0x9008D19f58AAbD9eD0D60971565AA8510560ab41')
-NATIVE_ETH_TOKEN_ADDRESS = Web3.to_checksum_address('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
-WETH_TOKEN_ADDRESS = Web3.to_checksum_address('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
-SDAI_TOKEN_ADDRESS = Web3.to_checksum_address('0x83F20F44975D03b1b09e64809B757c47f942BEeA')
+SETTLEMENT_CONTRACT_ADDRESS = Web3.to_checksum_address(
+    "0x9008D19f58AAbD9eD0D60971565AA8510560ab41"
+)
+NATIVE_ETH_TOKEN_ADDRESS = Web3.to_checksum_address(
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+)
+WETH_TOKEN_ADDRESS = Web3.to_checksum_address(
+    "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+)
+SDAI_TOKEN_ADDRESS = Web3.to_checksum_address(
+    "0x83F20F44975D03b1b09e64809B757c47f942BEeA"
+)
