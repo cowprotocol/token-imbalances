f7b0b33034077b0d38b9fd2f8511efe54983a542
Shubh Agarwal <mailtoshubh@gmail.com>
2024-07-07 02:17:35 -0400

    check if exists, small changes

--- src/config.py
+++ src/config.py
@@ -1,23 +1,24 @@
 import os
 import psycopg2
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Engine
 from dotenv import load_dotenv
+from urllib.parse import urlparse
+from psycopg2.extensions import connection as Psycopg2Connection
 from src.helper_functions import get_logger
 
 load_dotenv()
 ETHEREUM_NODE_URL = os.getenv("ETHEREUM_NODE_URL")
 GNOSIS_NODE_URL = os.getenv("GNOSIS_NODE_URL")
 
-ARBITRUM_NODE_URL = os.getenv("ARBITRUM_NODE_URL")
-SOLVER_SLIPPAGE_DB_URL = os.getenv("SOLVER_SLIPPAGE_DB_URL")
-
 CHAIN_RPC_ENDPOINTS = {"Ethereum": ETHEREUM_NODE_URL, "Gnosis": GNOSIS_NODE_URL}
 
-# sleep time can be configured here
-CHAIN_SLEEP_TIMES = {"Ethereum": 60.0, "Gnosis": 120.0}
+CHAIN_SLEEP_TIMES = {
+    "Ethereum": float(os.getenv("ETHEREUM_SLEEP_TIME")),
+    "Gnosis": float(os.getenv("GNOSIS_SLEEP_TIME")),
+}
 
 
-def create_read_db_connection(chain_name: str):
+def create_read_db_connection(chain_name: str) -> Engine:
     """function that creates a connection to the CoW db."""
     if chain_name == "Ethereum":
         read_db_url = os.getenv("ETHEREUM_DB_URL")
@@ -27,14 +28,18 @@ def create_read_db_connection(chain_name: str):
     return create_engine(f"postgresql+psycopg2://{read_db_url}")
 
 
-def create_write_db_connection():
+def create_write_db_connection() -> Psycopg2Connection:
     """Function that creates a connection to the write database."""
+
+    parsed_url = urlparse(os.getenv("SOLVER_SLIPPAGE_DB_URL"))
+
+    # Connect to the database
     write_db_connection = psycopg2.connect(
-        database="solver_slippage",
-        host=os.getenv("SOLVER_SLIPPAGE_HOST"),
-        user=os.getenv("SOLVER_SLIPPAGE_USER"),
-        password=os.getenv("SOLVER_SLIPPAGE_PASS"),
-        port=5432,
+        database=parsed_url.path[1:],
+        user=parsed_url.username,
+        password=parsed_url.password,
+        host=parsed_url.hostname,
+        port=parsed_url.port,
     )
     return write_db_connection
 
