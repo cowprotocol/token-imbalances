b09beae579fae96a0909e724f3ca3e5d7e3cf9d4
Shubh Agarwal <mailtoshubh@gmail.com>
2024-07-13 02:03:55 -0400

    no backend connection

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -3,8 +3,16 @@ This file contains some auxiliary functions
 """
 from __future__ import annotations
 import sys
+import os
 import logging
-from typing import Optional
+from typing import List, Optional, Tuple
+from dotenv import load_dotenv
+from hexbytes import HexBytes
+from web3 import Web3
+from src.constants import SETTLEMENT_CONTRACT_ADDRESS
+
+load_dotenv()
+NODE_URL = os.getenv("NODE_URL")
 
 
 def get_logger(filename: Optional[str] = None) -> logging.Logger:
@@ -45,3 +53,47 @@ def get_logger(filename: Optional[str] = None) -> logging.Logger:
         logger.addHandler(file_handler)
 
     return logger
+
+
+def get_web3_instance() -> Web3:
+    """
+    returns a Web3 instance for the given blockchain via chain name.
+    """
+    return Web3(Web3.HTTPProvider(NODE_URL))
+
+
+def get_finalized_block_number(web3: Web3) -> int:
+    """
+    Get the number of the most recent finalized block.
+    """
+    return web3.eth.block_number - 67
+
+
+def get_tx_hashes_blocks(
+    start_block: int, end_block: int, web3: Web3
+) -> List[Tuple[str, int]]:
+    """
+    Get all transaction hashes appended with corresponding block (tuple) transactions
+    involving the settlement contract.
+    """
+    tx_hashes_blocks = []
+
+    for block_number in range(start_block, end_block + 1):
+        block = web3.eth.get_block(block_number, full_transactions=True)
+        for tx in block.transactions:  # type: ignore[attr-defined]
+            if tx.to and tx.to.lower() == SETTLEMENT_CONTRACT_ADDRESS.lower():
+                tx_hashes_blocks.append((tx.hash.hex(), block_number))
+    return tx_hashes_blocks
+
+
+def get_auction_id(web3: Web3, tx_hash: str) -> int:
+    """
+    Method that finds an auction id given a transaction hash.
+    """
+    transaction = web3.eth.get_transaction(HexBytes(tx_hash))
+    call_data = transaction["input"]
+    call_data_bytes = bytes.fromhex(call_data[2:])  # convert hex string to bytes
+    auction_id = int.from_bytes(
+        call_data_bytes[-8:], byteorder="big"
+    )  # specify byteorder
+    return auction_id
