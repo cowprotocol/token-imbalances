ea286511f98bf29682697b35208ed1cbb9b53674
Shubh Agarwal <mailtoshubh@gmail.com>
2024-08-12 20:44:31 -0400

    deleting entries fixes if script restarts

--- src/sql/delete_entries_max_block.sql
+++ src/sql/delete_entries_max_block.sql
@@ -2,10 +2,10 @@ BEGIN;
 
 DELETE FROM raw_token_imbalances
 WHERE chain_name = :chain_name 
-AND block_number = :block_number;
+AND block_number >= :block_number;
 
 DELETE FROM slippage_prices
 WHERE chain_name = :chain_name
-AND block_number = :block_number;
+AND block_number >= :block_number;
 
 COMMIT;
