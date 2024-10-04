de14e80c81e76987736309194e3217720e98e907
Felix Henneke <felix.henneke@protonmail.com>
2024-10-04 16:32:57 +0200

    add tests

---
+++ tests/e2e/test_blockchain_data.py
@@ -0,0 +1,19 @@
+from os import getenv
+
+from dotenv import load_dotenv
+from web3 import Web3
+from src.helpers.blockchain_data import BlockchainData
+
+load_dotenv()
+
+
+def tests_get_tx_hashes_blocks():
+    web3 = Web3(Web3.HTTPProvider(getenv("NODE_URL")))
+    blockchain = BlockchainData(web3)
+    start_block = 20892118
+    end_block = start_block
+    res = blockchain.get_tx_hashes_blocks(start_block, end_block)
+    assert res[0] == (
+        "0xb75e03b63d4f06c56549effd503e1e37f3ccfc3c00e6985a5aacc9b0534d7c5c",
+        20892118,
+    )
