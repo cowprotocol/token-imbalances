4ea04079bf5c14d723874a44d8c5e57ae70e7056
Felix Henneke <felix.henneke@protonmail.com>
2024-10-09 16:50:37 +0200

    skip native price when fetching prices

--- src/price_providers/price_feed.py
+++ src/price_providers/price_feed.py
@@ -1,8 +1,12 @@
+from web3.types import HexStr
+
 from src.price_providers.coingecko_pricing import CoingeckoPriceProvider
 from src.price_providers.dune_pricing import DunePriceProvider
 from src.price_providers.moralis_pricing import MoralisPriceProvider
 from src.price_providers.endpoint_auction_pricing import AuctionPriceProvider
 from src.helpers.config import logger
+
+NATIVE_TOKEN = HexStr("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE")
 
 
 class PriceFeed:
@@ -22,7 +26,11 @@ class PriceFeed:
         """Function iterates over list of price provider objects and attempts to get a price."""
         for provider in self.providers:
             try:
-                price = provider.get_price(price_params)
+                price: float | None = None
+                if HexStr(price_params["token_address"]) == NATIVE_TOKEN:
+                    price = 1.0
+                else:
+                    price = provider.get_price(price_params)
                 if price is not None:
                     return price, provider.name
             except Exception as e:
