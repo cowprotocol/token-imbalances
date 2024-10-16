08cfefdd2c86686cb80771ca1697930d7eef03e1
harisang <harisangelidakis@gmail.com>
2024-10-17 00:51:09 +0300

    fix test

--- tests/e2e/test_imbalances.py
+++ tests/e2e/test_imbalances.py
@@ -19,7 +19,6 @@ def tests_process_single_transaction():
         "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": 0,
         "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2": 0,
         "0xEE2a03Aa6Dacf51C18679C516ad5283d8E7C2637": 275548164523,
-        "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE": 0,
     }
 
 
